40cec68c8c53a6971fff63d029f8c49a58a6520c
Kirk van Gorkom <kirk@forgeapps.com>
2014-02-21 16:52:04 -0800

    Add basic web hook echo

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"fmt"
 )
 
 func main() {
@@ -27,6 +28,29 @@ func main() {
 			} else {
 				io.WriteString(w, "No robot for that command yet :(")
 			}
+		}
+	})
+	
+	http.HandleFunc("/slack-webhook", func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err == nil {
+			message := new(robots.SlashCommand)
+			decoder := schema.NewDecoder()
+			err := decoder.Decode(message, r.PostForm)
+			if err != nil {
+				log.Println("Couldn't parse post request:", err)
+			}
+			//robot := GetRobot(command)
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			//io.WriteString(w, robot.Run(command))
+			
+			response := new(robots.IncomingWebhook)
+			response.Channel = message.Channel_ID
+			response.Text = fmt.Sprintf("I heard: @%s", message.Text)
+			response.Unfurl_Links = true
+			response.Parse = "full"
+			robots.MakeIncomingWebhookCall(response)
 		}
 	})
 
